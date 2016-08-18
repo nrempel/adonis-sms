bcf5db2ca80da33637deafb80cdefdc813f29c80
Nick Rempel <nicholas.rempel@servmetrics.com>
2016-08-18 12:42:45 -0700

    Change log output format for better legibility

--- src/SMS/drivers/Log.js
+++ src/SMS/drivers/Log.js
@@ -8,7 +8,10 @@ class Log {
 
   send (message, config) {
     if (config) this.config = config;
-    logger.info(`SMS log driver send. from=${message.from} to=${message.to} text=${message.text}`);
+    logger.info(`SMS log driver send. from=${message.from} to=${message.to}`);
+    logger.info('—————————————————————————————————————————————');
+    logger.info(message.text);
+    logger.info('—————————————————————————————————————————————');
     return Promise.resolve();
   }
 }
